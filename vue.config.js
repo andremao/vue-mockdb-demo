acf2328cbc48f97122ef20163dccb9b90930e0ef
andremao <andremao@192.168.0.106>
2020-07-15 14:29:54 +0800

    ok

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@ module.exports = {
     before(app) {
       // 判断是否为开发环境
       if (process.env.NODE_ENV.toUpperCase() === 'DEVELOPMENT') {
-        // 如果需要获取到 post 请求参数，把下面这行代码的注释解开
-        app.use(require('body-parser').json()); // 把 post 请求参数解析为 json 格式
-        app.use(require('@andremao/mockdb').middleware); // 挂上中间件
+        require('@andremao/mockdb').install(app);
       }
     },
   },
